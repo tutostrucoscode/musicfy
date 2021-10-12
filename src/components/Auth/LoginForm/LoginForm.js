3444081b001d17b51ab283c59adeeca2c25846c7
JoseFlorez <tiomix68@gmail.com>
2021-10-11 21:22:05 -0500

    Registro y Login
    Sistema de login y registro completado

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -8,34 +8,115 @@ import "./LoginForm.scss";
 
 export default function LoginForm(props) {
   const { setSelectedForm } = props;
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState(defaultValueForm());
+  const [formError, setFormError] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [userActive, setUserActive] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const handlerShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const onSubmit = () => {
-    console.log("Login..");
+    setFormError({});
+    let errors = {};
+    let formOK = true;
+
+    if (!validateEmail(formData.email)) {
+      errors.email = true;
+      formOK = false;
+    }
+    if (formData.password.length < 6) {
+      errors.password = true;
+      formOK = false;
+    }
+    setFormError(errors);
+
+    if (formOK) {
+      setIsLoading(true);
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(formData.email, formData.password)
+        .then((response) => {
+          setUser(response.user);
+          setUserActive(response.user.emailVerified);
+          if (!response.user.emailVerified) {
+            toast.warning(
+              "Para poder hacer login antes tienes que verificar la cuenta."
+            );
+          }
+        })
+        .catch((err) => {
+          handlerErrors(err.code);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
   };
 
   return (
     <div className="login-form">
       <h1>Musica para todos.</h1>
 
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} onChange={onChange}>
         <Form.Field>
           <Input
             type="text"
             name="email"
             placeholder="Correo electronico"
             icon="mail outline"
-            //error={}
+            error={formError.email}
           />
+          {formError.email && (
+            <span className="error-text">
+              Por favor, introduce un correo electronico valido.
+            </span>
+          )}
           <Input
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             placeholder="Contraseña"
             icon="eye"
-            //error={}
+            error={formError.password}
+            icon={
+              showPassword ? (
+                <Icon
+                  name="eye slash outline"
+                  link
+                  onClick={handlerShowPassword}
+                />
+              ) : (
+                <Icon name="eye" link onClick={handlerShowPassword} />
+              )
+            }
           />
+          {formError.password && (
+            <span className="error-text">
+              Por favor, la contraseña es menor a 5 caracteres.
+            </span>
+          )}
         </Form.Field>
-        <Button type="submit">Iniciarr Sesion</Button>
+        <Button type="submit" loading={isLoading}>
+          Iniciar Sesion
+        </Button>
       </Form>
+      {!userActive && (
+        <ButtonResetSendEmailVerification
+          user={user}
+          setIsLoading={setIsLoading}
+          setUserActive={setUserActive}
+        />
+      )}
       <div className="login-form__options">
         <p onClick={() => setSelectedForm(null)}>Volver</p>
         <p>
@@ -45,4 +126,58 @@ export default function LoginForm(props) {
       </div>
     </div>
   );
+}
+
+function ButtonResetSendEmailVerification(props) {
+  const { user, setIsLoading, setUserActive } = props;
+
+  const resendVerificationEmail = () => {
+    user
+      .sendEmailVerification()
+      .then(() => {
+        toast.success("Se ha enviado el email de verificacion.");
+      })
+      .catch((err) => {
+        handlerErrors(err.code);
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setUserActive(true);
+      });
+  };
+
+  return (
+    //rensend-verification-email
+    <div className="resend-verification-email">
+      <p>
+        Si no has enviado el email de verificacion puedes volver a enviarlo
+        haciendo click <span onClick={resendVerificationEmail}>aqui.</span>
+      </p>
+    </div>
+  );
+}
+
+function handlerErrors(code) {
+  switch (code) {
+    case "auth/wrong-password":
+      toast.warning("El usuario o la contraseña son incorrectas");
+      break;
+    case "auth/too-many-requests":
+      toast.warning(
+        "Has enviado demasiadas solicitudes de reenvio de email en muy poco tiempo"
+      );
+      break;
+    case "auth/user-not-found":
+      toast.warning("El usuario o la contraseña son incorrectas");
+      break;
+    default:
+      break;
+  }
+}
+
+function defaultValueForm() {
+  return {
+    email: "",
+    password: "",
+  };
 }
